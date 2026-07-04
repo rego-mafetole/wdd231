cf13020ccca6f13bedb349fe42fe76b27607c45d
rmafetole@byupathway.edu <rmafetole@gmail.com>
2026-07-04 01:52:20 +0100

    course page

---
+++ scripts/home-page.js
@@ -0,0 +1,13 @@
+// Store the selected elements that we are going to use.
+
+const navButton = document.querySelector('#ham-btn');
+const navLinks = document.querySelector('#nav-bar');
+
+// Toggle the show class on and off
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navLinks.classList.toggle('show');
+});
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
