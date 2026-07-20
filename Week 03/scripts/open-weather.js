098b1b7c7be8749e2068fb9ff1bc5515c9bfbee8
rmafetole@byupathway.edu <rmafetole@gmail.com>
2026-07-20 18:40:17 +0100

    index page

--- Week 03/scripts/open-weather.js
+++ Week 03/scripts/open-weather.js
@@ -3,4 +3,4 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon={lon}&appid={API key}';
