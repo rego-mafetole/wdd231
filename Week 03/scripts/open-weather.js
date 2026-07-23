42d3564baa31e4935608377c8ea753eeac3829c1
rmafetole@byupathway.edu <rmafetole@gmail.com>
2026-07-23 10:17:56 +0100

    open weather

--- Week 03/scripts/open-weather.js
+++ Week 03/scripts/open-weather.js
@@ -1,7 +1,8 @@
 // SELECT HTML ELEMENTS IN THE DOCUMENT
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const myTown = document.querySelector('#town')
+const myDescription = document.querySelector('#description');
+const myTemperature = document.querySelector('#temperature');
+const myGraphic = document.querySelector('#graphic');
 
 // CREATE REQUIRED VARIABLES FOR THE URL
 const myKey = "39f9082eab16f45ab47967b1ffdc201e"
@@ -9,7 +10,7 @@ const myLat = "49.75017104071407"
 const myLong = "6.636585097421277"
 
 // CONSTRUCT A FULL PATH USING TEMPLATE LITERALS
-const myURL = '//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial';
+const myURL = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`;
 
 // TRY TO GRAB THE CURRENT WEATHER DATA
 
@@ -19,7 +20,7 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data); // testing only
-            // displayResults(data); // uncomment when ready
+            displayResults(data); // uncomment when ready
         } else {
             throw Error(await response.text());
         }
@@ -28,4 +29,15 @@ async function apiFetch() {
     }
 }
 
+// DISPLAY THE JSON DATA ONTO MY WEB PAGE
+function displayResults(data) {
+    myTown.innerHTML = data.name
+    myDescription.innerHTML = data.weather[0].description
+    myTemperature.innerHTML = `${data.main.temp}&deg;F`
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    myGraphic.setAttribute('SRC', iconsrc)
+    myGraphic.setAttribute('alt', data.weather[0].description)
+}
+
+// START THE PROCESS
 apiFetch();
